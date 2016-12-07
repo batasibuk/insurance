e158a04c4088ecb8e862fd1c0161b2ec38dcbf80
batasibuk <batasibuk.com@gmail.com>
2016-12-08 00:17:15 +0800

    Add files via upload

---
+++ insurance.js
@@ -0,0 +1,33 @@
+Here's 10 insurance product Should you and your family Have
+
+Here's 10 insurance product Should you and your family Have – through life, each family would have been unable to escape from a problem to the family should be in hadapkan on a variety of complex problems is mainly about how to live out the rest of our own family members. Insurance is the right solution for you to tackle that problem. According to its kind, there are 10 insurance product which is very important for you to use as a form of self protection and the family. Following his review:
+
+Life Insurance
+Life insurance is a form of protection against self or family that you care about, especially if you are the backbone of the family and a source of finance for your family. In this type of life insurance product, you will be living in insure. We will not be able to predict bad things will happen to You later. To that end, life insurance will help your life when one moment will happen something yourself. Some of the benefits the other is life insurance this insurance will guarantee the financial condition of the family, can be combined with other insurance products, could be one of investment products for the future and as an anticipation of the unexpected thing.
+
+Health Insurance
+Health care costs more expensive, making you have to have health insurance. This will help You when unexpected circumstances happen to yourself. One of the health insurance provided a Government is the BPJS. Low premiums have BPJS and of course greatly relieve the public in General when medical treatment and cure the ailment. There are several different types of health insurance with a different policy, the policy will be membiaya You when you're in require for inpatient care in a hospital so that you will not be burdened because of all the costs of the hospital already on Your health insurance by civer
+
+Accident Insurance
+We will not be able to Dodge again in when the disaster come your way. Accident insurance can help to handle and prevent the discharge of the treatment costs. You need to be aware that there are some specific terms and conditions (contained in the insurance policy) that has been set by insurers. How is the definition of kecelakan occur for example, if you have been using proven mobile when driving, or being drunk while driving, then the accident insurance will not bear the brunt of the cost. Currently, the Government has also been providing insurance products on duty to handle traffic accidents named Services Raharja
+
+Home Insurance
+If you are mencicil a new home, You need home insurance mandatory. Typically, a host of housing credit products will directly provide protection in the form of home insurance. In selecting a home insurance policy, you should first carefully they offer. Whether to cover all damages or not. You need to know about home insurance, depending on the age of the building as well as the items that are in your home. Calculations must be accurate with Your insurance agent.
+
+Vehicle Insurance
+Many people assume that vehicle insurance is an optional that are not too important to insure. This is a wrong assumption. Even though you're a good driver and always be careful in driving, the accident could have happened to you anytime while You are on the road. Therefore, vehicle insurance can be your solution to solve it. Right now, the insurance premium vehicle light enough because it has a system of futures. You can pay the fee with cheaper per month and you can have the protection of yourself and your own vehicle
+
+Insurance Education
+If one type of person who desperately care about children, education insurance should not be missed. It is important for children to have a future. This insurance is a form of the preparation costs of education as well as protection of the soul. In case of accident, the insurance will indemnify the education on receipt by the beneficiary. The benefits that can be taken with the following insurance education is a guarantee of the future education of your children and even though the giver of the well-to-do have died, children's education continue to run and no doubt will be assured.
+
+Insurance Loss
+This insurance will provide liability on reseiko that will cause any harm. For example, theft, fire, etc. The accident is certainly not going to get You the predictions of when it will come. To that end, if you have a business with many employees and could just be having accidents while working, we recommend that you have insurance losses. Can you guarantee the property will be replaced when you got an accident
+
+Travel Insurance
+If you already have a travel insurance, of course the activities of travelling you will be pleasant and free from anxiety. This insurance will provide compensation if you travel when affected by replacing or provide emergency medical expenses, repatriation of the corpse as well as compensation for the heirs.
+
+Pension Insurance
+When you are retired, of course you've not been able to work again so that your income will decrease drastically. Therefore, this insurance can be your solution to resolve the issue. The benefits that can be drawn from this insurance is old age you will be more beautiful and pleasing to you will always feel calm in his life.
+
+Islamic Insurance
+Shariah-compliant products are already quite popular and known by the people of Indonesia. One of them is Islamic insurance. Islamic insurance is a financial protection for You based on the principles of Islamic jurisprudence. If you want an insurance product in accordance with the rules of Islamic Sharia insurance, this could be the solution the right choice for you. In addition to this insurance are not afraid of the haram or not, the Division result also more fair between customers and insurers. Thus the information on the 10 insurance product should you and your family Have. Hopefully this article can provide benefits to you.
